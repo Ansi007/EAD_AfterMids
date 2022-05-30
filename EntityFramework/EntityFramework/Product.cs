8741aed35e6478f29af2005d1ef8c1a4af4062af
Anns Shahbaz <annsshahbaz@gmail.com>
2022-05-30 10:44:12 +0500

    Scaffolding

---
+++ EntityFramework/EntityFramework/Product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EntityFramework
+{
+    public partial class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Type { get; set; } = null!;
+    }
+}
