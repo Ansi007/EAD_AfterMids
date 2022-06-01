c88dbdf57b459dbc457907e47201701e994615ec
Anns Shahbaz <annsshahbaz@gmail.com>
2022-06-01 10:10:39 +0500

    Step 1: Partial View Created and Added in Controller

--- Jquery/Jquery/Controllers/HomeController.cs
+++ Jquery/Jquery/Controllers/HomeController.cs
@@ -28,5 +28,11 @@ namespace Jquery.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public PartialViewResult NewsPartialView()
+        {
+            return PartialView("NewsPartialView");
+        }
+
     }
 }
