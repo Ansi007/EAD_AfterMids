17cabdab326f24b51be4c2e86e747cb17d7d3639
Anns Shahbaz <annsshahbaz@gmail.com>
2022-08-17 22:50:25 +0500

    code first

--- EntityFramework/EntityFramework/Program.cs
+++ EntityFramework/EntityFramework/Program.cs
@@ -1,15 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using EntityFramework;
-
 Console.WriteLine("Hello, World!");
-
-var context = new EntityFrameworkPracticeContext();
-//Product p = new Product();
-//while (true)
-//{
-    //p.Id = int.Parse(Console.ReadLine());
-    //p.Name = Console.ReadLine();
-    //p.Type = Console.ReadLine();
-    //context.Products.Add(p);
-   // context.SaveChanges();
-//}
---
+++ ImageUpload/ImageUpload/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using ImageUpload.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+
+namespace ImageUpload.Controllers
+{
+    public class HomeController : Controller
+    {
+        List<IFormFile> profilePictures;
+        private readonly IWebHostEnvironment _env;
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public HomeController(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult ImageUpload(IFormFile profileImage)
+        {
+            string webRootPath = _env.WebRootPath;
+            string path = Path.Combine(webRootPath, "img");
+
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            using (FileStream stream = new FileStream(Path.Combine(path, "ProfilePic.jpg"))
+            {
+                profileImage.CopyTo(stream);
+            };
+            return View("ProfilePictures", profilePictures);
+        }
+    }
+}
